fb7ee09bc52f9813ec2d6a77ef9b93f693af7a5c
ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>
2021-12-14 14:23:16 +0500

    VIRTUAL DESTRUCTOR
    Signed-off-by: ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>

---
+++ virtual destructor/virtual destructor.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Object
+{
+public:
+    Object() { printf("Object is created\n"); }
+    ~Object() { printf("Object is deleted\n"); }
+};
+
+class Base
+{
+public:
+    Base() { printf("Base object is created\n"); }
+    virtual ~Base() { printf("Base object is deleted\n"); }
+};
+
+class Desc : public Base
+{
+public:
+    Object  obj;
+    Desc() { printf("Desc object is created\n"); }
+    ~Desc() { printf("Desc object is deleted\n"); }
+};
+
+int main()
+{
+    Base* b = new Desc();
+    delete b;
+    return 0;
+}
