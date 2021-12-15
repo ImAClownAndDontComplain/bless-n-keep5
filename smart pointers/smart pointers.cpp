f6e8137d10879a78740ef3a0c657d6f21374926e
ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>
2021-12-15 07:59:48 +0500

    smart pointers
    Signed-off-by: ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>

---
+++ smart pointers/smart pointers.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class object {
+public:
+	object() {
+		printf("Object is created\n");
+	}
+	~object() {
+		printf("Object is deleted\n");
+	}
+};
+
+int main()
+{
+	object* o1 = new object();
+	object* o2 = o1;
+	delete o1;
+	//delete o2;
+	{
+		unique_ptr<object> p1(new object());
+		//unique_ptr<object> p2(p1); - impossible
+		unique_ptr<object> p3;
+		p3 = move(p1);
+		//p3.swap(p1)
+	}
+	{
+		shared_ptr<object> p1(new object());
+		shared_ptr<object> p2(p1);
+	}
+}
